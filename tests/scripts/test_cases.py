84e5568100459e8c214fe3cd633514fec211fa0b
Илья <lionson@mail.ru>
2020-12-09 23:22:26 +0200

    change test case: admin should find himself

--- tests/scripts/test_cases.py
+++ tests/scripts/test_cases.py
@@ -15,11 +15,11 @@ def check_managers_list(json_managers_list):
 
 @test_case(f'/managers/{test_managers.FooOwner.id}')
 def check_foo_owner(json_managers_list):
-    assert len(json_managers_list) == 3
+    assert len(json_managers_list) == 4
 
     json_managers_uuids = get_uuid_set(json_managers_list)
     test_managers_uuids = get_test_uuid_set(
-        [test_managers.One, test_managers.Two, test_managers.Three])
+        [test_managers.FooOwner, test_managers.One, test_managers.Two, test_managers.Three])
 
     assert json_managers_uuids == test_managers_uuids, "recived and expected data are not equal"
 
