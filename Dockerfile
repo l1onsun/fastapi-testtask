194b3bd29ae5a149ccff4c4eba4a69dc2f1922a7
Илья <lionson@mail.ru>
2020-12-09 23:17:52 +0200

    docker default: run gunicorn

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,4 @@ WORKDIR $WORKDIR
 RUN pip install pipenv
 RUN pipenv install --system --deploy --ignore-pipfile
 
-# CMD ["sleep", "300"] # overrideed by docker-compose
+CMD ["sh", "docker_cmd.sh"]
--- scripts/docker_cmd_test.sh
+++ scripts/docker_cmd_test.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-python -m scripts.switch_environment docker development
-python -m scripts.manage_database migrate seed-test
 gunicorn app.main:app -k uvicorn.workers.UvicornWorker -c config/gunicorn_conf.py
